013d52134732922b42cea270c3ca6e73756f7c08
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2018-12-17 04:09:03 +0530

    button material

--- src/auth/shared/shared.module.ts
+++ src/auth/shared/shared.module.ts
@@ -9,19 +9,26 @@ import { AuthformComponent } from './container/authform/authform.component';
 // Services
 import { AuthService } from './services/auth.service';
 
+// Material Module
+import { MatInputModule, MatButtonModule } from '@angular/material';
+
 export const ROUTES: Routes = [];
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   declarations: [
     AuthformComponent
   ],
   exports: [
-    AuthformComponent
+    AuthformComponent,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class SharedModule {
